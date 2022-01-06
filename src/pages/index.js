114683770a13c41c0d4b0cc71927bad25cdcbc60
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-01-06 02:10:42 +0200

    Site Metadata & Page Queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
+  const {title, description} = data.site.siteMetadata;
   return (
     <Layout>
       <section className={styles.header}>
         <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
+          <h2>{title}</h2>
+          <h3>{description}</h3>
           <p>UX designer & web developer based in Manchester.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
@@ -18,3 +19,16 @@ export default function Home() {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        title
+        description
+        copyright
+      }
+    }
+  }
+
+`
