00193c1b837ebd37b40481c862adbe26926714db
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-01-09 02:30:35 +0200

    Query Variables

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -5,17 +5,41 @@ import { graphql } from 'gatsby';
 
 import * as styles from '../styles/project-details.module.css'
 
-export default function projectDetails() {
+export default function projectDetails({ data }) {
+  console.log(data);
+  const {html} = data.markdownRemark;
+  const innrHtml = {__html: html}
+  const {title, stack, featuredImg} = data.markdownRemark.frontmatter;
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage image={getImage()}/> */}
+          <GatsbyImage image={getImage(featuredImg)} alt={title}/>
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div className={styles.html} dangerouslySetInnerHTML={innrHtml} />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+        stack
+      }
+    }
+  }
+`
